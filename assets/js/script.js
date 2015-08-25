a14f0f111700d2fda26ccf49c0a66d59b6a01482
Brian de Heus <brian@Brians-MacBook-Air.local>
2015-08-25 23:50:54 +0900

    Lots of files. And annoying DS store

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+(function (window) {
+
+	var submitButton      = $('#submit');
+	var keyForm           = $('#friendship-key');
+	var minecraftNameForm = $('#minecraft-name');
+
+	submitButton.on('click', function () {
+
+		var key          = keyForm.val();
+		var minecraftName = minecraftNameForm.val();
+
+		if (key.length === 0) {
+
+			alert('The friendship key should not be empty!');
+			return;
+
+		}
+
+		if (minecraftName.length === 0) {
+
+			alert('Your minecraft name should not be empty');
+			return;
+
+		}
+
+
+		$.post('/api/check-friendship', {minecraftName: minecraftName, key: key}, function (response, data) {
+
+			console.log('Response:', arguments);
+
+		});
+
+	});
+
+}(window));
--- index.js
+++ index.js
@@ -1,12 +1,75 @@
-var express   = require('express');
-var config    = require('./config.json');
-var templates = require('./lib/templates');
+var express    = require('express');
+var bodyParser = require('body-parser')
+var async      = require('async');
 
-var app     = express();
+var config     = require('./config.json');
+var templates  = require('./lib/templates');
+var mc         = require('./lib/mc');
+var whitelist  = require('./lib/whitelist');
+
+var app = express();
+app.use(express.static('assets')); //To load static assets without having to run a webserver
+app.use(bodyParser.urlencoded({ extended: true })); //Easier form parsing.
+app.use(bodyParser.json()); // Sure why not.
 
 app.get('/', function (req, res) {
 
   res.send(templates.TEMPLATES.base);
+
+});
+
+app.post('/api/check-friendship', function (req, res) {
+
+	var mcData = null;
+
+	function checkFriendshipKey(next) {
+
+		if (req.body.key !== config.friendship_key) {
+
+			next('The friendship key is invalid.');
+			return;
+
+		}
+
+		next();
+
+	}
+
+	function getMinecraftID(next) {
+
+		mc.findID(req.body.minecraftName, function (error, data) {
+
+			mcData = data;
+			next(error);
+
+		});
+
+	}
+
+	function checkIfAlreadyServerMember(next) {
+
+		whitelist.addToList(mcData, next);
+
+	}
+
+	async.series([
+
+		checkFriendshipKey,
+		getMinecraftID,
+		checkIfAlreadyServerMember
+
+	], function (error) {
+
+		if (error) {
+
+			res.send(error);
+			return;
+			
+		}
+
+		res.send('Accepted. Happy playing!')
+
+	});
 
 });
 
